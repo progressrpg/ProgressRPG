0452fbffd171dd1e3176ad6ae1df646c44f8f56a
gaidheal1 <gaidheal01@gmail.com>
2026-01-18 16:41:44 +0000

    fix: rebuild React

---
+++ static/frontend/assets/LogoutPage-Cv1YQLvk.js
@@ -0,0 +1,1 @@
+import{a,u as r,r as u,j as t}from"./main-Dz_-l1Dp.js";function n(){const{logout:e}=a(),s=r();return u.useEffect(()=>{e();const o=setTimeout(()=>{s("/")},800);return()=>clearTimeout(o)},[]),t.jsxs("div",{children:[t.jsx("h2",{children:"👋 Logging you out..."}),t.jsx("p",{children:"Please wait a moment."})]})}export{n as default};
