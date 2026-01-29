f499159afd93c894a3da0d06e3c5027c5f4e6c3f
gaidheal1 <gaidheal01@gmail.com>
2026-01-29 17:22:04 +0000

    fix: last few changes, rebuild react

--- static/frontend/assets/ButtonFrame-BYswzrdn.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:static/frontend/assets/ButtonFrame-BYswzrdn.js
-import{j as o}from"./main-D1vdwcto.js";const n="_buttonFrame_htxm4_1",r={buttonFrame:n};function e({children:t}){return o.jsx("div",{className:r.buttonFrame,children:t})}export{e as B};
-========
-import{j as o}from"./main-COlkBlnb.js";const n="_buttonFrame_htxm4_1",r={buttonFrame:n};function e({children:t}){return o.jsx("div",{className:r.buttonFrame,children:t})}export{e as B};
->>>>>>>> 0e43e0a (feat: simple svg map for current population centre (link in Profile page)):static/frontend/assets/ButtonFrame-DC7QqvEu.js
