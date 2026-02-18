52b0011d4c0ca6e3084c3bcbed2827284207a0d5
gaidheal1 <gaidheal01@gmail.com>
2026-02-18 10:20:26 +0000

    feat: additional mgmt commands

---
+++ users/management/commands/seed_superuser.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+import os
+
+
+class Command(BaseCommand):
+    help = "Creates a superuser from environment variables if it doesn't exist"
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+        password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+
+        if password is None:
+            self.stderr.write("DJANGO_SUPERUSER_PASSWORD env var not set")
+            return
+
+        if User.objects.filter(email=email).exists():
+            self.stdout.write(f"Superuser with email '{email}' already exists.")
+        else:
+            User.objects.create_superuser(email=email, password=password)
+            self.stdout.write(f"Superuser with email '{email}' created successfully.")
